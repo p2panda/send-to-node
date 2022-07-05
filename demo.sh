0b9734cd7e6466a967d56e255a305489ba613b88
Andreas Dzialocha <x12@adz.garden>
2022-07-05 15:11:00 +0200

    Make it work

--- demo.sh
+++ demo.sh
@@ -11,3 +11,4 @@ cat 008-update-conflict-2.toml | cargo run -- -k ./key-demo.txt -d 0020785779ab6
 cat 009-update-conflict-merge.toml | cargo run -- -k ./key-demo.txt -d 0020785779ab68f23aebad5b5cc35dea32f8cbea57a028ccdb3d96345e21d2473de1
 cat 010-schema-field-definition-venue.toml | cargo run -- -k ./key-demo.txt
 cat 011-schema-definition-event.toml | cargo run -- -k ./key-demo.txt
+cat 012-create-event.toml | cargo run -- -k ./key-demo.txt
