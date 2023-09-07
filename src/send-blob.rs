0caaddc880fe79e7289e1af04413efc9a4e969b6
adz <x12@adz.garden>
2023-09-07 10:57:15 +0200

    Update p2panda-rs, use bytes operation fields

--- src/send-blob.rs
+++ src/send-blob.rs
@@ -11,13 +11,12 @@ use indicatif::{HumanBytes, ProgressBar};
 use p2panda_rs::document::DocumentViewId;
 use p2panda_rs::operation::plain::PlainOperation;
 use p2panda_rs::operation::OperationBuilder;
+use p2panda_rs::schema::validate::MAX_BLOB_PIECE_LENGTH;
 use p2panda_rs::schema::SchemaId;
 use tokio::fs::File;
 use tokio::io::{AsyncReadExt, BufReader};
 
 use crate::client::Client;
-
-const BLOB_PIECE_LENGTH: usize = 256;
 
 /// Upload any file to a p2panda node.
 #[derive(Parser, Debug)]
@@ -59,7 +58,7 @@ async fn main() -> Result<()> {
         Some(guessed_type) => guessed_type.to_string(),
         None => "application/octet-stream".into(),
     };
-    let expected_blob_pieces = file_size / BLOB_PIECE_LENGTH as u64;
+    let expected_blob_pieces = file_size / MAX_BLOB_PIECE_LENGTH as u64;
     println!(
         "▶ File: \"{}\" ({}, {} blob pieces)",
         mime_type,
@@ -67,8 +66,8 @@ async fn main() -> Result<()> {
         expected_blob_pieces
     );
 
-    let mut reader = BufReader::with_capacity(BLOB_PIECE_LENGTH, file);
-    let mut buf: [u8; BLOB_PIECE_LENGTH] = [0; BLOB_PIECE_LENGTH];
+    let mut reader = BufReader::with_capacity(MAX_BLOB_PIECE_LENGTH, file);
+    let mut buf: [u8; MAX_BLOB_PIECE_LENGTH] = [0; MAX_BLOB_PIECE_LENGTH];
 
     let bar = ProgressBar::new(file_size);
     let mut blob_pieces: Vec<DocumentViewId> = Vec::with_capacity(expected_blob_pieces as usize);
@@ -80,10 +79,9 @@ async fn main() -> Result<()> {
             break;
         }
 
-        // @TODO: Use bytes operation values
         // Send blob piece to node
         let operation = OperationBuilder::new(&SchemaId::BlobPiece(1))
-            .fields(&[("data", "".into())])
+            .fields(&[("data", buf[..].into())])
             .build()?;
         let operation_id = client
             .sign_and_send(&key_pair, &PlainOperation::from(&operation))
